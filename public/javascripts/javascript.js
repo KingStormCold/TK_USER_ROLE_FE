89b2c2691630e0cee256a7839f5429ef5430d388
TuanKul <thaithanhtuan43@gmail.com>
2020-05-24 22:53:16 +0700

    create page user

--- public/javascripts/javascript.js
+++ public/javascripts/javascript.js
@@ -1,6 +1,45 @@
+$(document).ready(function(){
+    $('#example').DataTable( {
+        "lengthChange": false,
+        "pageLength": 15,
+        "autoWidth" : false
+    });
+    $('.dataTables_filter input').prop("placeholder", "Search");
+});
+
 $('#avatar-img').click(function(){
     $('.personal-info-header').slideToggle(200);
 });
 $('.icon-menu').click(function(){
     alert("CLick Click");
 });
+
+$('.main-auth-icon').click(function(){
+    if ($('.main-auth-icon').hasClass("fa-plus")) {
+        removeIconPlusAndAdIconMinus(this, '.main-auth-list');
+    } else {
+        removeIconMinusAndAdIconPlus(this, '.main-auth-list');
+    }
+});
+
+$('.menu-manager-icon').click(function(){
+    if ($('.menu-manager-icon').hasClass("fa-plus")) {
+        removeIconPlusAndAdIconMinus(this, '.main-menu-manager-list');
+    } else {
+        removeIconMinusAndAdIconPlus(this, '.main-menu-manager-list');
+    }
+});
+
+function removeIconMinusAndAdIconPlus(icon, ul){
+    $(ul).animate({"opacity": "1"}, 200 )
+    $(ul).show(1000);
+    $(icon).removeClass("fa-minus");
+    $(icon).addClass("fa-plus");
+};
+
+function removeIconPlusAndAdIconMinus(icon, ul){
+    $(ul).animate({ "opacity": "0.2" }, 200 );
+    $(ul).hide(1000);
+    $(icon).removeClass("fa-plus");
+    $(icon).addClass("fa-minus");
+};
--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/abc', function(req, res, next) {
-  res.render('admin/home', { title: 'Tuan 123' });
+router.get('/role', function(req, res, next) {
+  res.render('admin-home', { title: 'Tuan 123' });
 });
 
+router.get('/user', function(req, res, next) {
+  res.render('admin-user', { title: 'Tuan 123' });
+});
 module.exports = router;
